3406a0112f944a69d5cf3a7e4ab9a370b5ca761d
LongProDev <thanhlongn08@gmail.com>
2024-10-22 21:39:44 +0700

    Frontend Home ang Tours page
    Completed Home page frontend and preliminary Tours page frontend

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -24,7 +24,7 @@ const TourCard = ({tour}) => {
           <span className="tour__location d-flex align-items-center gap-1">
             <i className="ri-map-pin-line"></i>{city}
           </span>
-          <span className="tour__location d-flex align-items-center gap-1">
+          <span className="tour__rating d-flex align-items-center gap-1">
             <i className="ri-star-fill"></i>{avgRating ===0 ? null : avgRating}
             {totalRating === 0? 'Not rated' : (<span>({reviews.length})</span>)}
             
