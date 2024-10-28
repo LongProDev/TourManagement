ae0aa6c84d1a60bd97aa82908e306514d150c793
LongProDev <thanhlongn08@gmail.com>
2024-10-28 21:34:19 +0700

    Build API Backend
    Backend API Using NodeJs, MongoDB & JWT

---
+++ backend/src/routes/users.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { deleteUser, updateUser, getSingleUser, getAllUser} from "../controller/userController.js";
+import { verifyAdmin, verifyUser } from "../../utils/verifyToken.js";
+
+const router = express.Router();
+
+
+//update User
+router.put('/:id', verifyUser, updateUser);
+
+//delete User
+router.delete('/:id', verifyUser, deleteUser);
+
+//get single User
+router.get('/:id', verifyUser, getSingleUser);
+
+//get all User
+router.get('/', verifyAdmin, getAllUser);
+
+export default router
